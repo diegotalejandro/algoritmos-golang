2dd87de16e36630bf8262c9380fc0724986aafa5
diegotalejandro <diego.torreblanca2013@gmail.com>
2017-05-18 14:58:43 -0400

    Update tarea-2.go

--- tarea-2.go
+++ tarea-2.go
@@ -8,4 +8,52 @@ import(
 	"os"
 	"sync")
 
+func SCC(g grafo) [][]ID{//SCC: Strongly Connected Components
+	d:=newSCC()
+	for v:= range g.GetNodes(){
+		if _, ok:= d.index[v];c !ok{
+			scc(g,v,d)
+		}
+	}
+	return d.result
+}
 
+type sccData struct{
+
+}
+
+
+type node struct{
+	id string
+}
+var nodeCnt uint64
+func NewNode(id string) Node {
+	return &node{
+		id: id,
+	}
+}
+
+func (n *node) ID() ID{
+	return StringID(n.id)
+}
+
+func (n *node) String() string{
+	return n.id
+}
+
+type grafo struct{
+idparanodos map[ID]Node
+nodopararecursos map[ID]map[ID]float64
+nodoparaobjetivos map[ID]map[ID]float64
+}
+func newGrafo() *grafo{
+	return &grafo{
+		idparanodos: make(map[ID]Node),
+		nodopararecursos: make(map[ID]map[ID]float64),
+		nodoparaobjetivos: make(map[ID]map[ID]float64),
+	}
+}
+
+func NewGrafo() grafo{
+	return newGrafo()
+}
